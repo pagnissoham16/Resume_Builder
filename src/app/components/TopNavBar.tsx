f26f0f3bff59fb0346e505e500f108f7dc4fd773
pagnissoham16 <125887029+pagnissoham16@users.noreply.github.com>
2024-04-25 15:35:17 +0530

    Resume_Builder

---
+++ src/app/components/TopNavBar.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+import logoSrc from "public/logo.svg";
+import { cx } from "lib/cx";
+
+export const TopNavBar = () => {
+  const pathName = usePathname();
+  const isHomePage = pathName === "/";
+
+  return (
+    <header
+      aria-label="Site Header"
+      className={cx(
+        "flex h-[var(--top-nav-bar-height)] items-center border-b-2 border-gray-100 px-3 lg:px-12",
+        isHomePage && "bg-dot"
+      )}
+    >
+      <div className="flex h-10 w-full items-center justify-between">
+        <Link href="/">
+          <span className="sr-only">ProFolio</span>
+          {/* <Image
+            src={logoSrc}
+            alt="OpenResume Logo"
+            className="h-8 w-full"
+            priority
+          /> */}
+        </Link>
+        <nav
+          aria-label="Site Nav Bar"
+          className="flex items-center gap-2 text-sm font-medium"
+        >
+        </nav>
+      </div>
+    </header>
+  );
+};
